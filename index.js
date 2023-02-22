69bcaca452a2a07438c032d5208bb7d38349ea04
Thiago S Adriano <tadriano.net@gmail.com>
2023-02-22 06:53:58 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const readline = require('readline');
+
+// Generate random number between 1 and 100
+const secretNumber = Math.floor(Math.random() * 100) + 1;
+
+// Set up readline interface
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// Define the game function
+function playGame() {
+    rl.question('Guess a number between 1 and 100: ', (answer) => {
+        const guess = parseInt(answer);
+        if (isNaN(guess) || guess < 1 || guess > 100) {
+            console.log('Invalid input. Please enter a number between 1 and 100.');
+            playGame();
+            return;
+        }
+        if (guess === secretNumber) {
+            console.log('Congratulations, you guessed the number!');
+            rl.close();
+            return;
+        }
+        if (guess < secretNumber) {
+            console.log('Too low!');
+        } else {
+            console.log('Too high!');
+        }
+        playGame();
+    });
+}
+
+// Start the game
+console.log('I\'m thinking of a number between 1 and 100...');
+playGame();
---
+++ test.js
@@ -0,0 +1,41 @@
+const { expect, test } = require('@jest/globals');
+const { createInterface } = require('readline');
+const { stdout, stdin } = require('process');
+const { resolve } = require('path');
+
+// Import the playGame function from the main file
+const { playGame } = require(resolve(__dirname, 'main.js'));
+
+// Mock the readline interface
+jest.mock('readline');
+
+describe('playGame', () => {
+    test('correctly handles a correct guess', () => {
+        // Set up mock readline interface to return the secret number
+        const mockReadline = createInterface({ input: stdin, output: stdout });
+        mockReadline.question.mockImplementationOnce((_, callback) => callback('50'));
+        // Call playGame and check the output
+        expect.assertions(1);
+        return playGame().then(result => expect(result).toEqual('Congratulations, you guessed the number!'));
+    });
+
+    test('correctly handles an incorrect guess', () => {
+        // Set up mock readline interface to return a too low guess, then a correct guess
+        const mockReadline = createInterface({ input: stdin, output: stdout });
+        mockReadline.question.mockImplementationOnce((_, callback) => callback('25'));
+        mockReadline.question.mockImplementationOnce((_, callback) => callback('50'));
+        // Call playGame and check the output
+        expect.assertions(1);
+        return playGame().then(result => expect(result).toEqual('Congratulations, you guessed the number!'));
+    });
+
+    test('correctly handles invalid input', () => {
+        // Set up mock readline interface to return an invalid guess, then a correct guess
+        const mockReadline = createInterface({ input: stdin, output: stdout });
+        mockReadline.question.mockImplementationOnce((_, callback) => callback('abc'));
+        mockReadline.question.mockImplementationOnce((_, callback) => callback('50'));
+        // Call playGame and check the output
+        expect.assertions(1);
+        return playGame().then(result => expect(result).toEqual('Congratulations, you guessed the number!'));
+    });
+});
